d68732678c7fe7fa21c8b29650a340c22dfcb5eb
NatanBoz <natan.bozydaj@gmail.com>
2024-03-24 17:49:50 +0100

    Add a button changing grid and hover over item

--- jscript.js
+++ jscript.js
@@ -1,7 +1,42 @@
 let container = document.querySelector(".container");
 
+function createGrid() {
 for (let i=0; i<16*16; i++) {
     let gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item")
+    gridItem.classList.add("grid-item");
     container.appendChild(gridItem);
+    }
+}
+
+createGrid();
+
+let number = null;
+
+let btn = document.querySelector("button")
+btn.onclick = function () {
+    while (number === null || isNaN(number) || number < 0 || number > 100) {
+        number = prompt("How many squares do you want in your grid? (0-100)");
+        if (number === null) {
+            alert("You canceled or closed the prompt.");
+            break;
+        }
+
+        number = parseInt(number);
+
+        if (isNaN(number) || number < 0 || number > 100) {
+            alert("Invalid number.");
+        }
+    }
+    
+    container.innerHTML = "";
+
+    let itemSize = (960 / number) + "px";
+
+    for (let i = 0; i < number* number; i++) {
+        let gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.style.width = itemSize;
+        gridItem.style.height = itemSize;
+        container.appendChild(gridItem);
+    }
 }
